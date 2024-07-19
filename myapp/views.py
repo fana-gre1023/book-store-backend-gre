b67c84f4c797cc7bb928bdd7f38ff266ccea8a62
fana-gre1023 <gregoryfrank.beal@outlook.com>
2024-07-19 16:28:45 -0400

    .

--- myapp/views.py
+++ myapp/views.py
@@ -7,7 +7,7 @@ from.serializers import BookSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-class BookViewSet(viewsets.ModelViewSet):
+class BookViewSet(viewsets.ModelViewSet): 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
