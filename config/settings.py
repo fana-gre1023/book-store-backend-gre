2dae840c6ba77d1c17a27eadd17ef5a77043ab2e
fana-gre1023 <gregoryfrank.beal@outlook.com>
2024-07-21 15:35:25 -0400

    fix

--- config/settings.py
+++ config/settings.py
@@ -10,15 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path
 from decouple import config 
 import dj_database_url
+from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
